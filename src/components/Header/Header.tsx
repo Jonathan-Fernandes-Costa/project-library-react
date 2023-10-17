769ce6d74a155dfd75a562885de846e99d919275
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-10-17 15:13:06 -0300

    feat: Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between p-4 bg-white shadow-md">
+      <div className="flex-shrink-0">
+        <span className="text-2xl">☰</span>
+      </div>
+
+      <div className="flex items-center space-x-4">
+        <span className="bg-green-500 text-white px-2 py-1 rounded-full text-sm">
+          ADMIN
+        </span>
+
+        <button className="text-blue-500">Sair</button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
