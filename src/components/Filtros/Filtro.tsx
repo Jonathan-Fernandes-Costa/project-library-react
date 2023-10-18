4148fbcc09d6b3206a2a43fd4efd14c34697bb00
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-10-18 16:36:39 -0300

    feat: componente Filtro

---
+++ src/components/Filtros/Filtro.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+function Filtro() {
+    const [pesquisa, setPesquisa] = useState('');
+    const [categoria, setCategoria] = useState('Todos');
+    return (
+        <div className="p-5">
+            <div className='p-3 bg-white shadow-md rounded-md'>
+                <h2 className="block shadow-2xl mb-4 text-xl font-semibold">Filtros</h2>
+                <div className='flex'>
+                    <div className="w-2/3 mb-4 mr-4">
+                        <label className="mb-2 font-medium ">Pesquisa <span className="text-sm text-gray-500">(Id, Título, Subtítulo, Código, Editora, Autor, Sinopse, Ano Edição)</span></label>
+                        <input
+                            type="text"
+                            placeholder="Pesquisar..."
+                            className="w-full p-2 border rounded-md"
+                            value={pesquisa}
+                            onChange={e => setPesquisa(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="w-1/3 mb-4 ">
+                        <label className="mb-2 font-medium text-gray-600">Livro Categoria</label>
+                        <select
+                            className="w-full p-2 border rounded-md"
+                            value={categoria}
+                            onChange={e => setCategoria(e.target.value)}
+                        >
+                            <option value="Todos">Todos</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Filtro
--- src/pages/Livros.tsx
+++ src/pages/Livros.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from '../components/Header/Header'
+import Header from '../components/Header'
+import Filtro from '../components/Filtros/Filtro'
 
 function Livros() {
   return (
     <div>  
-        <Header></Header>
+        <Header/>
+        <Filtro/>
     </div>
   )
 }
